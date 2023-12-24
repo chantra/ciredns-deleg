72edc8445a4c259c5b1c5ae63c7c2e209d4eaeda
Manu Bretelle <chantr4@gmail.com>
2023-12-23 18:24:00 -0800

    Add the list of records to a response
    If the response contains at least 1 NS record which matches a zone, we will
    add the responses to the Auth section.
    
    Signed-off-by: Manu Bretelle <chantr4@gmail.com>

--- deleg.go
+++ deleg.go
@@ -18,8 +18,60 @@ type Deleg struct {
 
 // ServeDNS implements the plugin.Handler interface.
 func (d Deleg) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg) (int, error) {
-	return plugin.NextOrFailure(d.Name(), d.Next, ctx, w, r)
+
+	pw := NewResponsePrinter(w, d)
+
+	return plugin.NextOrFailure(d.Name(), d.Next, ctx, pw, r)
 }
 
 // Name implements the Handler interface.
 func (d Deleg) Name() string { return "deleg" }
+
+// ResponsePrinter wrap a dns.ResponseWriter and will write example to standard output when WriteMsg is called.
+type ResponsePrinter struct {
+	dns.ResponseWriter
+	d Deleg
+}
+
+// NewResponsePrinter returns ResponseWriter.
+func NewResponsePrinter(w dns.ResponseWriter, d Deleg) *ResponsePrinter {
+	return &ResponsePrinter{w, d}
+}
+
+func (d Deleg) matches(owner string) string {
+	for _, z := range d.zones {
+		if dns.CountLabel(owner) == dns.CountLabel(z) && dns.IsSubDomain(z, owner) {
+			return z
+		}
+	}
+	return ""
+}
+
+// WriteMsg calls the underlying ResponseWriter's WriteMsg method and prints "example" to standard output.
+func (r *ResponsePrinter) WriteMsg(res *dns.Msg) error {
+	// Nothing to be done here.
+	if len(res.Ns) == 0 {
+		return r.ResponseWriter.WriteMsg(res)
+	}
+
+	for _, auth := range res.Ns {
+		owner := auth.Header().Name
+		rtype := auth.Header().Rrtype
+		// not an NS record, tentatively try the next records
+		if rtype != dns.TypeNS {
+			continue
+		}
+		zone := r.d.matches(owner)
+		//Let's assume that if there is a NS record, then there are all for the same owner name
+		if zone == "" {
+			return r.ResponseWriter.WriteMsg(res)
+		}
+		// We have a matching zone, adding the RRs to the Auth section
+		res.Ns = append(res.Ns, r.d.responses...)
+		// and we are done.
+		break
+	}
+
+	// Following back to writing the original response
+	return r.ResponseWriter.WriteMsg(res)
+}
--- deleg_test.go
+++ deleg_test.go
@@ -2,8 +2,10 @@ package deleg
 
 import (
 	"context"
+	"reflect"
 	"testing"
 
+	"github.com/coredns/coredns/plugin"
 	"github.com/coredns/coredns/plugin/pkg/dnstest"
 	"github.com/coredns/coredns/plugin/test"
 
@@ -22,4 +24,121 @@ func TestDeleg(t *testing.T) {
 		// request not handle, should be passed to next (non-existent) plugin.
 		t.Errorf("Expected an error")
 	}
+}
+
+// TestDelegResponse tests the response of the deleg plugin.
+// checking that it adds the responses to the NS section of the response
+// when expected.
+func TestDelegResponse(t *testing.T) {
+
+	testCases := []struct {
+		name        string
+		auth        []dns.RR
+		zones       []string
+		responses   []dns.RR
+		shouldMatch bool
+	}{
+		// Standard response with an NS record matching one of the zones
+		{
+			"Matching zone",
+			[]dns.RR{test.NS("example.org. 3600 IN NS ns1.example.org.")},
+			[]string{"example.org."},
+			[]dns.RR{test.A("example.org. 3600 IN A 127.0.0.1")},
+			true,
+		},
+		// Check that we only add the records once.
+		{
+			"Matching zone multiple times",
+			[]dns.RR{test.NS("example.org. 3600 IN NS ns1.example.org."), test.NS("example.org. 3600 IN NS ns2.example.org.")},
+			[]string{"example.org."},
+			[]dns.RR{test.A("example.org. 3600 IN A 127.0.0.1")},
+			true,
+		},
+		// Standard response with an NS record not matching any of the zones
+		{
+			"Not matching zone",
+			[]dns.RR{test.NS("example.com. 3600 IN NS ns1.example.org.")},
+			[]string{"example.org."},
+			[]dns.RR{test.A("example.org. 3600 IN A 127.0.0.1")},
+			false,
+		},
+		// Standard response with an NS record matching one of the zones, the matching zone is not the first in the list.
+		{
+			"Multizone match",
+			[]dns.RR{test.NS("example.org. 3600 IN NS ns1.example.org.")},
+			[]string{"example.com.", "example.org."},
+			[]dns.RR{test.A("example.org. 3600 IN A 127.0.0.1")},
+			true,
+		},
+		// Standard response with an NS record matching one of the zones, the matching zone is not the first in the list.
+		// and we have other records along the NS record.
+		{
+			"Multizone match",
+			[]dns.RR{test.A("example.com. 3600 IN A 127.0.0.1"), test.NS("example.org. 3600 IN NS ns1.example.org.")},
+			[]string{"example.com.", "example.org."},
+			[]dns.RR{test.A("example.org. 3600 IN A 127.0.0.1")},
+			true,
+		},
+		// Standard response with an NS record matching one of the zones, the aded record is not matching the owner of the NS record. Expected as we do not validate this currently.
+		{
+			"Multizone match wrong owner in response",
+			[]dns.RR{test.NS("example.com. 3600 IN NS ns1.example.org.")},
+			[]string{"example.com.", "example.org."},
+			[]dns.RR{test.A("example.org. 3600 IN A 127.0.0.1")},
+			true,
+		},
+		// Standard response with a record matching one of the zones BUT it is not an NS record so we do not add anything.
+		{
+			"Multizone match wrong type in response",
+			[]dns.RR{test.A("example.com. 3600 IN A 127.0.0.1")},
+			[]string{"example.com.", "example.org."},
+			[]dns.RR{test.AAAA("example.org. 3600 IN AAAA ::1")},
+			false,
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			req := new(dns.Msg)
+			req.SetQuestion("www.example.org.", dns.TypeA)
+			d := &Deleg{
+				zones:     tc.zones,
+				responses: tc.responses,
+			}
+
+			d.Next = BackendHandler(tc.auth)
+
+			ctx := context.TODO()
+
+			rec := dnstest.NewRecorder(&test.ResponseWriter{})
+
+			d.ServeDNS(ctx, rec, req)
+
+			expectedNs := append(tc.auth, tc.responses...)
+			if tc.shouldMatch {
+				if !reflect.DeepEqual(rec.Msg.Ns, expectedNs) {
+					t.Errorf("Expecting %s got %s", expectedNs, rec.Msg.Ns)
+				}
+			} else {
+				if !reflect.DeepEqual(rec.Msg.Ns, tc.auth) {
+					t.Errorf("Expecting %s got %s", tc.auth, rec.Msg.Ns)
+				}
+			}
+
+		})
+	}
+
+}
+
+// A backend handler that will add a list of records to the NS section of the response.
+func BackendHandler(auth []dns.RR) plugin.Handler {
+	return plugin.HandlerFunc(func(ctx context.Context, w dns.ResponseWriter, r *dns.Msg) (int, error) {
+		m := new(dns.Msg)
+		m.SetReply(r)
+		m.Response = true
+
+		m.Ns = append(m.Ns, auth...)
+
+		w.WriteMsg(m)
+		return dns.RcodeSuccess, nil
+	})
 }
